03f5d58ad681d5a89e99ead0d9fe34f9565a8a46
jcvdav <juanarlos.villader@gmail.com>
2016-06-12 17:49:40 -0700

    added server section of the app, its up and running

--- bvi_app/app.R
+++ bvi_app/app.R
@@ -6,21 +6,37 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 
-ui=fluidPage(
-  titlePanel(""),
+ui=(fluidPage(
+  titlePanel("Biological Value Index calculator"),
   sidebarLayout(
-    sidebarPanel(fileInput(inputId="indata",
-                           label="Choose a file to upload",
-                           accept = c(".xslx",
-                                      ".csv",
-                                      ".xls"))),
-    tabsetPanel(tabPanel("Table",
-                         mainPanel(tableOutput("table"))),
-              tabPanel("Plot",
-                       mainPanel(plotOutput("plot")))
-              )
+    sidebarPanel(
+      fileInput("indata", "Chose a *.csv file to upload",
+                accept = c(
+                  'text/csv',
+                  'text/comma-separated-values',
+                  'text/tab-separated-values',
+                  'text/plain',
+                  '.csv',
+                  '.tsv'
+                )
+      ),
+      checkboxInput('header', 'Header', TRUE),
+      radioButtons('sep', 'Separator',
+                   c(Semicolon=";",
+                     Comma=",",
+                     Tab="\t"),
+                   ";")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Table", tableOutput("table")),
+        tabPanel("Plot", plotOutput("plot"))
+      )
     )
   )
+))
+
+options(shiny.maxRequestSize = 1000*1024^2)
 
 # Define server logic required to draw a histogram
 server = function(input, output) {
@@ -32,7 +48,12 @@ server = function(input, output) {
        return(NULL)
      }
      
-     data=read.csv(inFile$datapath, row.names=1, sep=";")
+     data = read.csv(inFile$datapath,
+                     header = input$header,
+                     sep = input$sep,
+                     quote = input$quote,
+                     row.names = 1)
+     
      bvi(data=data, p=0.95, other=T)
    })
    
@@ -44,8 +65,17 @@ server = function(input, output) {
        return(NULL)
      }
      
-     data=read.csv(inFile$datapath, row.names=1, sep=";")
-     bvi_plot(bvi(data))
+     data = read.csv(inFile$datapath,
+                     header = input$header,
+                     sep = input$sep,
+                     quote = input$quote,
+                     row.names = 1)
+     
+     plot1=bvi_plot(bvi(data))
+     
+     plot1+
+       theme_bw()+
+       scale_fill_brewer(palette="Paired")
    })
 }
 
